7f987018501df9dececf8c48d60417fab3c7a3c2
Louis Warren <louis@lsw.nz>
2017-04-01 22:03:07 +1300

    Add cp

--- shpy.py
+++ shpy.py
@@ -1,4 +1,16 @@
 import os
+import shutil
+
+def cp(*paths, recursive=None):
+    *sources, dest = paths
+    if len(sources) == 1 and not os.path.isdir(dest):
+        if not os.path.isdir(sources[0]) or recursive:
+            shutil.copyfile(sources[0], dest)
+    else:
+        for source in sources:
+            if not os.path.isdir(source) or recursive:
+                target = os.path.join(dest, os.path.basename(source))
+                shutil.copyfile(source, target)
 
 def ls(path=None, all=None):
     if all:
@@ -17,7 +29,8 @@ def mv(*paths):
         os.rename(sources[0], dest)
     else:
         for source in sources:
-            os.rename(source, os.path.join(dest, os.path.basename(source)))
+            target = os.path.join(dest, os.path.basename(source))
+            os.rename(source, target)
 
 def touch(path):
     with open(path, 'a'):
